ef7aeb5bf15f9fd8d2aadaaefc9a51220c376fea
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-03-22 01:02:27 -0700

    display search and button

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-class App extends React.Component{
-  render(){
-    return(
-      <header>
-        <h1>Hi</h1>
-      </header>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchData: []
+    }
+  }
+  handleSearch = async (event) => {
+    //prevents default refresh
+    event.preventDefault();
+    //get the data from the API
+    let searchRequest = await axios.get('https://swapi.dev/api/people/?page=1');
+    //save that date into state
+    this.setState({
+      searchData: searchRequest.data.results
+    })
+    console.log(this.state)
+  }
+  render() {
+    let searchListItems = this.state.searchData.map((character,idx) => {
+      return<li key={idx}>{character.name}</li>
+    })
+    return (
+      <>
+        <header>
+          <h1>CityExplorer</h1>
+        </header>
+        <form>
+          <input></input>
+          <button onClick={this.handleSearch}>
+            Explore
+          </button>
+        </form>
+        <ul>
+          {searchListItems}
+        </ul>
+      </>
     )
   }
 }

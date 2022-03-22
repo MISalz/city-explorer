8e18d0cd89dea6e7e0efa30d4158edada050324b
Michelle Salazar <Michelle.Salazar010Gmail.com>
2022-03-22 01:46:55 -0700

    display search and button

--- src/App.js
+++ src/App.js
@@ -6,37 +6,40 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      searchData: []
+      searchData: {}
     }
   }
-  handleSearch = async (event) => {
+  getCityData = async (event) => {
     //prevents default refresh
     event.preventDefault();
     //get the data from the API
-    let searchRequest = await axios.get('https://swapi.dev/api/people/?page=1');
+    let searchData = await axios.get(`https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_LOCATIONIQ_API_KEY}&q=${this.state.city}&format=json`);
     //save that date into state
-    this.setState({
-      searchData: searchRequest.data.results
-    })
-    console.log(this.state)
   }
-  render() {
-    let searchListItems = this.state.searchData.map((character,idx) => {
-      return<li key={idx}>{character.name}</li>
+    handleCityInput = (event) => {
+    this.setState({
+      city: event.target.value
     })
+  }
+  
+  render() {
+    // let searchListItems = this.state.searchData.map((character, idx) => {
+    //   return <li key={idx}>{character.name}</li>
+    // })
     return (
       <>
         <header>
           <h1>CityExplorer</h1>
         </header>
-        <form>
-          <input></input>
-          <button onClick={this.handleSearch}>
+        <form onSubmit={this.searchData}>
+          <label>Pick a city!</label>
+          <input type="text" onInput={this.handleCityInput} />
+          <button type="submit">
             Explore
           </button>
         </form>
         <ul>
-          {searchListItems}
+          {/* {searchListItems} */}
         </ul>
       </>
     )
